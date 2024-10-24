b859947d8da1aacb0b851a47c12ef8201c4bbc63
mei higashi <mei@meihigashi.com>
2024-10-23 23:25:51 -0700

    近日公開！

--- src/routes/blog/oct/+page.svelte
+++ src/routes/blog/oct/+page.svelte
@@ -1,2 +1,94 @@
 <title>october's blog post</title>
 <p>meows scarily</p>
+
+<figure style="margin: 0;">
+    <img
+        src="/images/blog/oct/daifuku.jpg"
+        style="max-width: 300px; width: 300px; height: 300px; object-fit: cover;"
+        alt="daifuku in little tokyo"
+    />
+    <figcaption style="text-align: left;">
+        <em>sweetness is in the air, and i am placed in the present!</em>
+    </figcaption>
+</figure>
+
+<p>
+    october has been wonderful and joyous! i’ve been enjoying the balance in
+    between the lingering of summer heat and the emergence of the winter chill.
+    oh, how i’m excited to deck out in oversized heavyweight hoodies, thermal
+    pants under cargo pants, and overly platform-ed sneakers. yay!
+</p>
+
+<figure style="margin: 0;">
+    <img
+        src="/images/blog/oct/bunny.jpg"
+        style="max-width: 300px; width: 300px; height: 300px; object-fit: cover;"
+        alt="daifuku in little tokyo"
+    />
+    <figcaption style="text-align: left;">
+        <em></em>
+    </figcaption>
+</figure>
+
+<h2>
+    i might taste you on my lips forever
+</h2>
+<ul>
+    <li>
+        kinako daifuku from fugestu-do
+    </li>
+    <li>
+        black truffle pasta from il pastaio
+    </li>
+    <li>
+        banana pie and white coffee ice cream at uchi
+    </li>
+    <li>
+        smith’s strawberry lip salve
+    </li>
+    <li>
+        sleepytime tea for drowsy bear time
+    </li>
+    <li>
+        hot coffee kisses, brown sugar on my tongue
+    </li>
+    <li>
+        yuzu tamago pan
+    </li>
+    <li>
+        al-dente udon swimming in light dashi broth
+    </li>
+</ul>
+
+<figure style="margin: 0;">
+    <img
+        src="/images/blog/oct/bags.jpg"
+        style="max-width: 300px; width: 300px; height: 300px; object-fit: cover;"
+        alt="daifuku in little tokyo"
+    />
+    <figcaption style="text-align: left;">
+        <em></em>
+    </figcaption>
+</figure>
+
+<figure style="margin: 0;">
+    <img
+        src="/images/blog/oct/truffle.jpg"
+        style="max-width: 300px; width: 300px; height: 300px; object-fit: cover;"
+        alt="daifuku in little tokyo"
+    />
+    <figcaption style="text-align: left;">
+        <em></em>
+    </figcaption>
+</figure>
+
+<figure style="margin: 0;">
+    <img
+        src="/images/blog/oct/uchi.jpg"
+        style="max-width: 300px; width: 300px; height: 300px; object-fit: cover;"
+        alt="daifuku in little tokyo"
+    />
+    <figcaption style="text-align: left;">
+        <em></em>
+    </figcaption>
+</figure>
