896e165d3092ba88d48516b143ea86089a352fc5
mei higashi <mei@meihigashi.com>
2024-12-11 21:51:22 -0800

    with them by young thuuuug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<link rel="icon" type="image/jpg" href="icon.jpg" />
+<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="aura evangelist ( ᵘ ᵕ ᵘ ⁎)" />
 <title>maisyland</title>
